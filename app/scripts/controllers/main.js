25997380c200467fbf18dd7ce1689201f2f29b43
tan <haitanle88@gmail.com>
2019-02-19 09:31:09 -0500

    new module dependency quiz

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name q1UpAndRunningApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the q1UpAndRunningApp
+ */
+angular.module('q1UpAndRunningApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

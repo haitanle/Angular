a2dbaeb7818d878347f1753f1c5a807d6e5679b1
tan <haitanle88@gmail.com>
2019-02-19 21:13:53 -0500

    add directives for interaction

--- udaciMeal/FEF-UdaciMeals-Angular-master/app/scripts/controllers/menu.js
+++ udaciMeal/FEF-UdaciMeals-Angular-master/app/scripts/controllers/menu.js
@@ -9,8 +9,28 @@
  */
 angular.module('udaciMealsAngularApp')
   .controller('MenuCtrl', function () {
-      this.id = 'strawberry-pudding';
-      this.name = 'Strawberry Pudding';
-      this.img = 'strawberry-pudding.jpg';
-      this.rating = 5;
+      this.items = [
+        {id : 'chicken-pudding',
+        name : 'Strawberry Pudding',
+        img : 'blackberry-water.jpg',
+        rating : 2
+      },
+        {id : 'strawberry-pudding',
+        name : 'Strawberry Pudding',
+        img : 'strawberry-yogurt.jpg',
+        rating : 2
+      },
+        {id : 'bananan-pudding',
+        name : 'banana Pudding',
+        img : 'bbq-pork-combo.jpg',
+        rating : 2
+      }
+      ];
+
+      this.increment = function(item){
+        item.rating++;
+      },
+       this.decrement = function(item){
+        item.rating--;
+      }
   });
